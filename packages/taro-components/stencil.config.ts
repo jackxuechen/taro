5fe19603e905b14522e17662aca67b114e660092
liujie <jackxuechen@163.com>
2021-08-16 18:14:00 +0800

    feat: 修改video组件

--- packages/taro-components/stencil.config.ts
+++ packages/taro-components/stencil.config.ts
@@ -3,8 +3,9 @@ import { sass } from '@stencil/sass'
 const { jsWithTs: tsjPreset } = require('ts-jest/presets')
 
 export const config: Config = {
-  namespace: 'taro-components',
+  namespace: 'my-taro-components',
   globalStyle: './node_modules/weui/dist/style/weui.min.css',
+  srcDir: 'src_lj',
   plugins: [
     sass()
   ],
@@ -15,11 +16,11 @@ export const config: Config = {
     }
   ],
   bundles: [
-    { components: ['taro-picker-core', 'taro-picker-group'] },
-    { components: ['taro-checkbox-core', 'taro-checkbox-group-core'] },
-    { components: ['taro-radio-core', 'taro-radio-group-core'] },
-    { components: ['taro-swiper-core', 'taro-swiper-item-core'] },
-    { components: ['taro-video-core', 'taro-video-control', 'taro-video-danmu'] }
+    // { components: ['taro-picker-core', 'taro-picker-group'] },
+    // { components: ['taro-checkbox-core', 'taro-checkbox-group-core'] },
+    // { components: ['taro-radio-core', 'taro-radio-group-core'] },
+    // { components: ['taro-swiper-core', 'taro-swiper-item-core'] },
+    { components: ['my-taro-video-core', 'my-taro-video-control', 'my-taro-video-danmu'] }
   ],
   buildEs5: 'prod',
   testing: {
